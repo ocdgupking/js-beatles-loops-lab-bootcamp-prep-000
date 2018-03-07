90f01db7f16d03afba8eb8661882e7e9f058a84b
guptaco@ymail.com <guptaco@ymail.com>
2018-03-07 02:02:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(musicians, instruments){
   var bands = {};
-  for (i = 0; i < musicians.length; i++ ){
+  for (var i = 0; i < musicians.length; i++ ){
     bands.push("${musicians[i]} plays ${instruments[i]}")
   }
   return bands
