82c6d1cb3b33f2fc6df6e08bcacb7f87d4c072a3
guptaco@ymail.com <guptaco@ymail.com>
2018-03-07 02:23:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function theBeatlesPlay(musicians, instruments){
 
 function johnLennonFacts(facts){
   var i = 0
-  var updateFacts = []
+  var updateFacts = [];
   while (facts[i]){
     return (facts[i] + "!!!")
     
